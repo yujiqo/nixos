06340037558a3fd8f15c2f27bf2b3db09a4a2a48
yujiqo <danilastolyarov97@gmail.com>
2026-09-22 16:19:36 +0500

    symlink kitty configs

--- modules/home-manager/symlinks.nix
+++ modules/home-manager/symlinks.nix
@@ -6,4 +6,5 @@ in
   xdg.configFile."git/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/git/config";
   xdg.configFile."niri".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/niri";
   xdg.configFile."fastfetch".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/fastfetch";
+  xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/kitty";
 }
