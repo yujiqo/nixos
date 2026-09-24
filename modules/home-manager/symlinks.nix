3e881ace63be0dc644c0b0e41440552498ee5ff5
yujiqo <danilastolyarov97@gmail.com>
2026-09-24 19:07:16 +0500

    remove unused comments

--- modules/home-manager/symlinks.nix
+++ modules/home-manager/symlinks.nix
@@ -2,16 +2,6 @@
   yurice = "${config.home.homeDirectory}/.nixos/yurice/config";
   dotfiles = "${config.home.homeDirectory}/.nixos/dotfiles/config";
 in {
-#  xdg.configFile."niri".source = config.lib.file.mkOutOfStoreSymlink "${yurice}/niri";
-#  xdg.configFile."fastfetch".source = config.lib.file.mkOutOfStoreSymlink "${yurice}/fastfetch";
-#  xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink "${yurice}/kitty";
-#  xdg.configFile."fish".source = config.lib.file.mkOutOfStoreSymlink "${yurice}/fish";
-#  xdg.configFile."starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${yurice}/starship.toml";
-#  xdg.configFile."tmux".source = config.lib.file.mkOutOfStoreSymlink "${yurice}/tmux";
-#  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${yurice}/nvim";
-
-#  xdg.configFile."git/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/git/config";
-#  xdg.configFile."opencode/opencode.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/opencode/opencode.jsonc";
   xdg.configFile."niri" = {
     source = config.lib.file.mkOutOfStoreSymlink "${yurice}/niri";
     force = true;
